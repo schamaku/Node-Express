4f8b5d997be8a910a193498fca1c92bb5e077567
nidhi reddy <nidhireddy@nidhis-MBP.fios-router.home>
2018-02-07 17:41:51 -0500

    Express Server

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 const hostname = 'localhost';
 const port = 3000;
 
@@ -11,6 +13,10 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 app.use('/dishes', dishRouter);
+
+app.use('/promotions', promoRouter);
+
+app.use('/leaders', leaderRouter);
 
 app.use(express.static(__dirname+ '/public'));
 
--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -30,4 +30,30 @@ dishRouter.route('/')
 	res.end('Deleteing all the dishes');
 });
 
+dishRouter.route('/:dishId')
+.all((req,res,next) => {
+	res.statusCode = 200;
+	res.setHeader('Content-Type','text/plain');
+	next();
+})
+
+.get((req,res,next) => {
+    res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
+})
+
+.post((req, res, next) => {
+  res.statusCode = 403;
+  res.end('POST operation not supported on /dishes/'+ req.params.dishId);
+})
+
+.put((req, res, next) => {
+  res.write('Updating the dish: ' + req.params.dishId);
+  res.end('\n'+ 'Will update the dish: ' + req.body.name + 
+        ' with details: ' + req.body.description);
+})
+
+.delete((req, res, next) => {
+    res.end('Deleting dish: ' + req.params.dishId);
+});
+
 module.exports = dishRouter;
